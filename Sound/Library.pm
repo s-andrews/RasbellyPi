7895f64139508ea6ad3f19e3ed323eb75fd0b785
Simon Andrews <simon@proteo.me.uk>
2016-04-11 20:05:55 +0100

    Added code to collect and return sound collections

--- Sound/Library.pm
+++ Sound/Library.pm
@@ -1,16 +1,66 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use Sound::SoundCollection;
 
 package Sound::Library;
 
 
-sub new () {
-  return bless {};
+sub new {
+
+  my $debug = 1;
+  
+  my ($package,$base_dir) = @_;
+
+  $debug and warn "Getting sounds from $base_dir\n";
+  
+  # List the directories under the base - each of these will be a sound collection
+  my @subdirs = <$base_dir/*>;
+
+  $debug and warn "Found ".scalar @subdirs." subdirectories\n";
+  
+  my $data = [];
+  bless $data;
+
+  foreach my $directory (@subdirs) {
+
+    $debug and warn "Looking at directory $directory\n";
+    
+    next unless (-d $directory);
+    my @files = <$directory/*>;
+
+    $debug and warn "Found ".scalar @files." files\n";
+    
+    my @sound_files;
+
+    foreach my $file (@files) {
+      if ($file =~ /(\.wav$|\.mp3$)/) {
+	push @sound_files,$file;
+      }
+    }
+
+    $debug and warn "Found ".scalar @sound_files." sound files\n";
+
+    
+    if (@sound_files) {
+      push @$data, new Sound::SoundCollection(\@sound_files);
+    }
+  }
+  
+  return $data;
 }
 
-sub play_sound () {
-  system ("aplay Sounds/Doorbell/DingDong.wav") == 0 or warn "Failed to play sound";
+sub play_sound {
+
+  my ($obj) = @_;
+
+  # TODO: Work out which collections are valid and pick the best one
+
+  my $collection = $obj->[int rand (scalar @$obj)];
+
+  my $file = $collection->get_file();
+  
+  system ("aplay", $file) == 0 or warn "Failed to play '$file'";
   
 }
 
